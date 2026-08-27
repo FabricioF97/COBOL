c797a53e9a15fa1c488c6dd425c95467b3d2c256
FabricioF97 <faa.ferreira97@gmail.com>
2026-08-26 22:36:33 -0300

    Atualiza projeto COBOL

--- Funcionario.cbl
+++ Funcionario.cbl
@@ -1,1 +1,79 @@
-    
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ContagemFuncionario.
+       AUTHOR. Fabricio Ferreira.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT FUNCIONARIOS ASSIGN TO "FUNCIONARIOS.txt"
+               ORGANIZATION IS LINE SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD FUNCIONARIOS.
+       01 DETALHEFUNCIONARIO.
+           05 MATRICULA-FUNCIONARIO  PIC 9(5).
+           05 NOME-FUNCIONARIO.
+               10 PRIMEIRO-NOME      PIC X(10).
+               10 ULTIMO-NOME        PIC X(12).
+           05 GENERO                 PIC X(1).
+
+       WORKING-STORAGE SECTION.
+       01 CONTADORES.
+           05 TOTAL-HOMENS           PIC 9(3) VALUE 0.
+           05 TOTAL-MULHERES         PIC 9(3) VALUE 0.
+
+       01 LEITURA-FINALIZADA         PIC X VALUE "N".
+
+       PROCEDURE DIVISION.
+       INICIO.
+           DISPLAY "========================="
+           DISPLAY "Contagem de Funcionarios"
+           DISPLAY "========================="
+
+      * Abrir arquivo
+           OPEN INPUT FUNCIONARIOS
+
+      * Processamento dos registros do arquivo
+           PERFORM UNTIL LEITURA-FINALIZADA = "S"
+
+               READ FUNCIONARIOS
+                   AT END
+                       MOVE "S" TO LEITURA-FINALIZADA
+
+                   NOT AT END
+                       INSPECT PRIMEIRO-NOME
+                           REPLACING ALL " " BY LOW-VALUES
+
+                       INSPECT ULTIMO-NOME
+                           REPLACING ALL " " BY LOW-VALUES
+
+                       DISPLAY MATRICULA-FUNCIONARIO
+                               " " GENERO " "
+                               PRIMEIRO-NOME " "
+                               ULTIMO-NOME
+
+                       IF GENERO = "M"
+                           ADD 1 TO TOTAL-HOMENS
+                       ELSE
+                           IF GENERO = "F"
+                               ADD 1 TO TOTAL-MULHERES
+                           END-IF
+                       END-IF
+
+               END-READ
+
+           END-PERFORM
+
+      * Fechar arquivo
+           CLOSE FUNCIONARIOS
+
+      * Exibir resumo
+           DISPLAY "============================="
+           DISPLAY "Resumo"
+           DISPLAY "Total Homens....: " TOTAL-HOMENS
+           DISPLAY "Total Mulheres..: " TOTAL-MULHERES
+           DISPLAY "============================="
+
+           STOP RUN.
+           
